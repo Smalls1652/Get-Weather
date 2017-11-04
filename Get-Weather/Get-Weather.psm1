e06d7c4ffe8dc0aeb5b4a8cec62a3895fdb7c407
Tim Small <smalls1652@gmail.com>
2017-11-04 16:37:28 -0400

    Formatted script.

--- Get-Weather/Get-Weather.psm1
+++ Get-Weather/Get-Weather.psm1
@@ -17,10 +17,13 @@
 	Show the next 8 day forecast.
 	
 	.PARAMETER Hourly
-  Show the hourly forecast for the rest of the day.
+  	Show the hourly forecast for the rest of the day.
   
-  .PARAMETER Config
-  Rerun the API Key config setup.
+  	.PARAMETER Config
+  	Rerun the API Key config setup.
+
+	.PARAMETER ClearDB
+	Clear all DB files.
 	
 	.PARAMETER Force
 	Force an update to the local DB of the location. Warning: This counts as an API call to Dark Sky.
@@ -31,10 +34,10 @@
 	
 	.EXAMPLE
 	# Get weather for a specific location through a zipcode and get the 8 day forecast.
-  Get-Weather -ZipCode 36602 -Forecast
+  	Get-Weather -ZipCode 36602 -Forecast
   
-  .EXAMPLE
-  # Force a refresh of the local DB for a zipcode and get the hourly forecast.
+  	.EXAMPLE
+  	# Force a refresh of the local DB for a zipcode and get the hourly forecast.
 	Get-Weather -ZipCode 36602 -Hourly -Force
 	#>
 	
@@ -45,8 +48,8 @@
         [string]$Address,
         [switch]$Forecast,
         [switch]$Hourly,
-		[switch]$Config,
-		[switch]$ClearDB,
+        [switch]$Config,
+        [switch]$ClearDB,
         [switch]$Force
 	
     )
@@ -100,24 +103,21 @@
 
 
         $googleData = (Invoke-RestMethod "https://maps.googleapis.com/maps/api/geocode/json?address=$Address&key=$googlegeocodeAPI").results
-		if ($googleData.Length -gt 1)
-		{
-			Write-Warning "More than one result returned.`n"
-			$i = 0
-			foreach ($option in $googleData)
-			{
-				$i++
-				$optionName = $option.formatted_address
-				Write-Host  "$i. $optionName"
-			}
+        if ($googleData.Length -gt 1) {
+            Write-Warning "More than one result returned.`n"
+            $i = 0
+            foreach ($option in $googleData) {
+                $i++
+                $optionName = $option.formatted_address
+                Write-Host  "$i. $optionName"
+            }
 			
-			while ((($chosenOption = Read-Host -Prompt "Select an option") -gt $i) -or !($chosenOption))
-			{
-				Write-Warning "Out of range. Please select and option within the range."
-			}
+            while ((($chosenOption = Read-Host -Prompt "Select an option") -gt $i) -or !($chosenOption)) {
+                Write-Warning "Out of range. Please select and option within the range."
+            }
 
-			$googleData = $googleData[$chosenOption - 1]
-		}
+            $googleData = $googleData[$chosenOption - 1]
+        }
         $geoLat = $googleData.geometry.location.lat
         $geoLong = $googleData.geometry.location.lng
 
@@ -160,19 +160,19 @@
 	
             $DayForecast = @{
 	
-                "Day"                 = $weektoday;
+                "Day"          = $weektoday;
 	
-                "Summary"             = $day.Summary;
+                "Summary"      = $day.Summary;
 	
-                "High (F)"                = ([math]::Round($day.temperatureMax)).ToString() + [char]0x00b0;
+                "High (F)"     = ([math]::Round($day.temperatureMax)).ToString() + [char]0x00b0;
 	
-                "Low (F)"                 = ([math]::Round($day.temperatureMin)).ToString() + [char]0x00b0;
+                "Low (F)"      = ([math]::Round($day.temperatureMin)).ToString() + [char]0x00b0;
 	
                 "Precip Prob." = $day.precipProbability.ToString() + "%";
 	
-                "Sunrise"             = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($day.sunriseTime)) | Get-Date -Format "hh:mm tt";
+                "Sunrise"      = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($day.sunriseTime)) | Get-Date -Format "hh:mm tt";
 	
-                "Sunset"              = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($day.sunsetTime)) | Get-Date -Format "hh:mm tt";
+                "Sunset"       = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($day.sunsetTime)) | Get-Date -Format "hh:mm tt";
             }
 	
             [pscustomobject]$DayForecast
@@ -183,32 +183,31 @@
         $HourlyOutput = foreach ($hour in $dsData.hourly.data) {
             $HourlyForecast = @{
 				
-				"Day" = If (([timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($hour.Time)) | Get-Date) -eq (Get-Date -Hour 0 -Minute 0 -Second 0 -Millisecond 0)) { "Today" } else { ([timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($hour.Time)) | Get-Date -Format "dddd").ToString() };
+                "Day"          = If (([timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($hour.Time)) | Get-Date) -eq (Get-Date -Hour 0 -Minute 0 -Second 0 -Millisecond 0)) { "Today" } else { ([timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($hour.Time)) | Get-Date -Format "dddd").ToString() };
 
-                "Hour"                = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($hour.Time)) | Get-Date -Format "hh:mm tt";
+                "Hour"         = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($hour.Time)) | Get-Date -Format "hh:mm tt";
 					
-                "Summary"             = $hour.Summary;
+                "Summary"      = $hour.Summary;
 					
-                "Temp (F)"         = ([math]::Round($hour.temperature)).ToString() + [char]0x00b0;
+                "Temp (F)"     = ([math]::Round($hour.temperature)).ToString() + [char]0x00b0;
 	
                 "Precip Prob." = $hour.precipProbability.ToString() + "%";
 	
-                "Pressure"            = $hour.pressure.ToString() + "mb";
+                "Pressure"     = $hour.pressure.ToString() + "mb";
 	
-                "Wind Speed"          = ([math]::Round($hour.windSpeed)).ToString() + " MPH"
+                "Wind Speed"   = ([math]::Round($hour.windSpeed)).ToString() + " MPH"
 	
             }
 					
             [pscustomobject]$HourlyForecast
         }
-        [pscustomobject]$HourlyOutput | Select-Object -Property "Day","Hour", "Summary", "Temp (F)", "Pressure", "Wind Speed", "Precip Prob." | Format-Table -AutoSize
-	}
-	elseif ($ClearDB) {
-		foreach ($file in (Get-ChildItem -Path $PSScriptRoot -Filter "*.xml"))
-		{
-			$file.Delete()
-		}
-	}
+        [pscustomobject]$HourlyOutput | Select-Object -Property "Day", "Hour", "Summary", "Temp (F)", "Pressure", "Wind Speed", "Precip Prob." | Format-Table -AutoSize
+    }
+    elseif ($ClearDB) {
+        foreach ($file in (Get-ChildItem -Path $PSScriptRoot -Filter "*.xml")) {
+            $file.Delete()
+        }
+    }
     else {
         $todaySunrise = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($dsData.daily.data[0].sunriseTime)) | Get-Date -Format "hh:mm tt";
         $todaySunset = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($dsData.daily.data[0].sunsetTime)) | Get-Date -Format "hh:mm tt";
@@ -218,25 +217,25 @@
 	
         $currentSummary = @{
 	
-            "Location"            = $cityLocation;
+            "Location"           = $cityLocation;
 	
-            "Last Updated"        = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($dsData.currently.time));
+            "Last Updated"       = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($dsData.currently.time));
 	
-            "Current Temp (F)" = ([math]::Round($dsData.currently.temperature)).ToString() + [char]0x00b0;
+            "Current Temp (F)"   = ([math]::Round($dsData.currently.temperature)).ToString() + [char]0x00b0;
 	
-            "Current Conditions"  = $dsData.currently.summary;
+            "Current Conditions" = $dsData.currently.summary;
 	
-            "Pressure"            = $dsData.currently.pressure.ToString() + " mb"; 
+            "Pressure"           = $dsData.currently.pressure.ToString() + " mb"; 
 	
-            "Wind Speed"          = ([math]::Round($dsData.currently.windSpeed)).ToString() + " MPH";
+            "Wind Speed"         = ([math]::Round($dsData.currently.windSpeed)).ToString() + " MPH";
 	
-            "Sunrise"             = $todaySunrise + $sunriseOccured;
+            "Sunrise"            = $todaySunrise + $sunriseOccured;
 	
-            "Sunset"              = $todaySunset + $sunsetOccured;
+            "Sunset"             = $todaySunset + $sunsetOccured;
 	
-            "Next Hour"           = $dsData.minutely.summary;
+            "Next Hour"          = $dsData.minutely.summary;
 	
-            "Next 48 Hours"       = $dsData.hourly.summary
+            "Next 48 Hours"      = $dsData.hourly.summary
         }
         return [pscustomobject]$currentSummary | Select-Object -Property "Location", "Last Updated", "Current Temp (F)", "Current Conditions", "Pressure", "Wind Speed", "Sunrise", "Sunset", "Next Hour", "Next 48 Hours"
     }
